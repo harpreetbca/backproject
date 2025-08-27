181ccaef3c0f01222cf11225c3f811953ef2cfd0
harpreetbca <preet4har6@gmail.com>
2025-08-27 21:01:22 +0530

    106

--- backend/routes/apiproduct.js
+++ backend/routes/apiproduct.js
@@ -1,9 +1,10 @@
-import { getprduct, createproduct, updateproduct, deleteproduct } from "../controller/product.controller.js";
+import { getprduct, getSingleProduct, createproduct, updateproduct, deleteproduct } from "../controller/product.controller.js";
 import express from "express"
 
 const route = express.Router();
 
 route.get("/getproduct", getprduct);
+route.get("/getproduct/:id", getSingleProduct);
 route.post("/createproduct", createproduct);
 route.put("/updateproduct/:id", updateproduct);
 route.delete("/deleteproduct/:id", deleteproduct);
